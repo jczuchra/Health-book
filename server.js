8a891a503128fec828964682070c05dd7dd76926
jczuchra <czuchra.jakub@gmail.com>
2019-06-03 11:10:27 +0200

    Sequelize connection with postre heroku

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(adminRoutes);
 sequelize
     .sync()
     .then(result => {
-        app.listen(3000);
+        app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
--- util/db/database.js
+++ util/db/database.js
@@ -1,10 +1,22 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('jczuchr1', 'jczuchr1', 'omCBheoJRPhqhhaC', {
-    dialect: 'mysql',
-    host: 'mysql.agh.edu.pl',
-    port: '3306'
-});
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    const sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        port: match[4],
+        host: match[3],
+        logging: true //false
+    })
+}
+else {
+    const sequelize = new Sequelize('jczuchr1', 'jczuchr1', 'omCBheoJRPhqhhaC', {
+        dialect: 'mysql',
+        host: 'mysql.agh.edu.pl',
+        port: '3306'
+    });
+}
 
 
 module.exports = sequelize;
