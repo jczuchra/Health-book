e23cd131819b218c6817b32803b7feb7bf23c6ec
jczuchra <czuchra.jakub@gmail.com>
2019-06-03 11:19:40 +0200

    Fix postgre db problem

--- util/db/database.js
+++ util/db/database.js
@@ -1,24 +1,11 @@
 const Sequelize = require('sequelize');
 
-let sequelize;
-
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        port: match[4],
-        host: match[3],
-        logging: true //false
-    })
-}
-else {
-    sequelize = new Sequelize('jczuchr1', 'jczuchr1', 'omCBheoJRPhqhhaC', {
-        dialect: 'mysql',
-        host: 'mysql.agh.edu.pl',
-        port: '3306'
-    });
-}
-
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+})
 
 module.exports = sequelize;
