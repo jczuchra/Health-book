a65f610bef5d26eb17590ac85575a838fab52654
jczuchra <czuchra.jakub@gmail.com>
2019-06-03 19:31:39 +0200

    Change db

--- util/db/database.js
+++ util/db/database.js
@@ -1,21 +1,21 @@
 const Sequelize = require('sequelize');
 
-if (process.env.DATABAS_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-            ssl: true
-        }
-    });
-}
-else {
-    sequelize = new Sequelize('jczuchr1', 'jczuchr1', 'omCBheoJRPhqhhaC', {
-        dialect: 'mysql',
-        host: 'mysql.agh.edu.pl',
-        port: '3306'
-    });
-}
+// if (process.env.DATABAS_URL) {
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
+// }
+// else {
+//     sequelize = new Sequelize('jczuchr1', 'jczuchr1', 'omCBheoJRPhqhhaC', {
+//         dialect: 'mysql',
+//         host: 'mysql.agh.edu.pl',
+//         port: '3306'
+//     });
+// }
 
 
 module.exports = sequelize;
