60446c5dadfcf59304ab3aac4d703b504355ce8d
jczuchra <czuchra.jakub@gmail.com>
2019-06-15 21:47:18 +0200

    Add session log

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.session && !req.session.user.admin)
+    if (req.session && req.session.user && !req.session.user.admin)
         res.redirect('/404')
     next();
 }
--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
+    console.log('SESJA', req.session);
     if (req.session && !req.session.isLoggedIn)
         res.redirect('/login');
     next();
